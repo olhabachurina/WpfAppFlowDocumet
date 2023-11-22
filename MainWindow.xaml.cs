e535bd3c440ed0a6da043a26f11acc194c049de6
Olga Bachurina <bachuri12@ukr.net>
2023-11-23 01:42:53 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LibVLCSharp.Shared;
+using LibVLCSharp.WPF;
+
+namespace WpfAppFlowDocumet
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        private void PreviousPage_Click(object sender, RoutedEventArgs e)
+        {
+            ScrollViewer scrollViewer = FindVisualChild<ScrollViewer>(flowDocumentReader);
+            if (scrollViewer != null)
+            {
+                scrollViewer.LineUp();
+            }
+        }
+      
+
+        private void NextPage_Click(object sender, RoutedEventArgs e)
+        {
+            ScrollViewer scrollViewer = FindVisualChild<ScrollViewer>(flowDocumentReader);
+            if (scrollViewer != null)
+            {
+                scrollViewer.LineDown();
+            }
+        }
+
+        private static T FindVisualChild<T>(DependencyObject parent) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(parent); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(parent, i);
+                if (child != null && child is T)
+                {
+                    return (T)child;
+                }
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                    {
+                        return childOfChild;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
